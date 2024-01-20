968819ee593011306d5f01c7113832fc64f00d6a
Elena Thomas <elenafayethomas@gmail.com>
2024-01-20 13:56:57 +0000

    fix(src): migrate pages and components to src

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,14 +1,5 @@
-function MyButton() {
+export function Button() {
   return (
     <button id="button" title="Click here to open dropdown menu" onclick="myFunction()"><span id="menuUnderline" >Menu</span></button>
-  );
-}
-
-export default function MyApp() {
-  return (
-    <div>
-      <MyButton />
-      <h1>Thomas Family Blog</h1> 
-    </div>
   );
 }

52deb46b133cd54040dcc46cd6491c02c054fb7e
Elena <116371000+lenakita@users.noreply.github.com>
2024-01-20 14:01:36 +0000

    fix(src): migrate pages and components to src (#1)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 
 import './App.css';
-
-function MyButton() {
-       return (
-         <button id="button" title="Click here to open dropdown menu" onclick="myFunction()"><span id="menuUnderline" >Menu</span></button>
-       );
-     }
+import { Button } from 'components/buttons';
+import { Navigation } from 'components/navbar';
 
 function MyMenu() {
-       return (
-              <div id="menu">
-                        <ul>
-                                <li><a className="nav-link" href = "April_2024.html"  title = "Click here to go to April 2024 blogs">April 2024</a></li>
-                                <li><a className="nav-link" href = "May_2024.html"  title = "Click here to go to May 2024 blogs">May 2024</a></li>
-                                <li><a className="nav-link" href = "June_2024.html" id="menu" title = "Click here to go to June 2024 blogs">June 2024</a></li>
-                        </ul>
-              </div>
-       );
+  return (
+    <div id="menu">
+      <ul>
+        <li><a className="nav-link" href = "April_2024.html"  title = "Click here to go to April 2024 blogs">April 2024</a></li>
+        <li><a className="nav-link" href = "May_2024.html"  title = "Click here to go to May 2024 blogs">May 2024</a></li>
+        <li><a className="nav-link" href = "June_2024.html" id="menu" title = "Click here to go to June 2024 blogs">June 2024</a></li>
+      </ul>
+    </div>
+  );
 }
 
 function ProfileJill() {
@@ -24,22 +20,22 @@ function ProfileJill() {
     name: 'Jill',
     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
     imageSize: 90,
-    };
+  };
     
-    return (
-      <>
+  return (
+    <div>
       <h1>{user.name}</h1>
       <img
-       className="avatar"
-       src={user.imageUrl}
-       alt={'Photo of ' + user.name}
-        style={{
-         width: user.imageSize,
-         height: user.imageSize
-       }}
-  />
-          </>
-         )
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+          style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </div>
+  )
 }
 
 
@@ -47,12 +43,12 @@ function ProfileJill() {
 export default function App() {
   return (
 <body>
-    <div className="App">
-    <MyButton />
-    <MyMenu />
+  <div className="App">
+    <Button />
+    <Navigation />
     <h1 id="the_Thomas_Family_Blog">Thomas Family Blog</h1>
     <ProfileJill />
-    </div>
+  </div>
 </body>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+export function Navigation() {
+  return (
+    <div id="menu">
+      <ul>
+        <li><a className="nav-link" href = "April_2024.html"  title = "Click here to go to April 2024 blogs">April 2024</a></li>
+        <li><a className="nav-link" href = "May_2024.html"  title = "Click here to go to May 2024 blogs">May 2024</a></li>
+        <li><a className="nav-link" href = "June_2024.html" id="menu" title = "Click here to go to June 2024 blogs">June 2024</a></li>
+      </ul>
+    </div>
+  );
+}

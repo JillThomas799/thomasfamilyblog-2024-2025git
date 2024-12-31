4a14951ff49bf94b86e0d5b9f6417be836f49eee
Jill Thomas <jillthomas532016@outlook.com>
2024-12-31 17:23:49 +0000

    Addition January 2025 blog

--- src/January2025.js
+++ src/January2025.js
@@ -7,7 +7,7 @@ import Snowstorm from 'components/snowstorm'
 
 export default function January2025() {
     return (
-        <div>
+        <div className="mainJanuary">
             <div div className="January2025Header" > January 2025
                <button id="backbuttonDecember">
                         <Link to="/" className="backbuttonLink" style={{color:"black"}}>Back</Link>
@@ -15,6 +15,28 @@ export default function January2025() {
 <Snowstorm />
 <CatLennie />
         </div>
+
+         <section className="dateJanuary" style={{marginTop:'0%'}}>January 1st - 31st 2025</section> 
+                     <section className="newsheaderJanuary"> WISHING EVERYONE A VERY HAPPY NEW YEAR!</section> 
+                    <section className="newsJanuary" style={{textAlign:"left"}}>
+                   Welcome to our January blog. This month, as it is January, we have returned to our snowy scene. However, given Lennie does not like the snow
+                   and he likes to keep warm we have created an igloo for him and a heated mat for him to lie on. He is in this animation busy eating and enjoying his dinner.
+                   <br />
+                   <br />
+                   It has also been entertaining with Lennie as he likes to sleep in his basket right by the patio window and look outside to survey his domain.
+                   Often we will see birds hopping towards the glass and looking in at Lennie, and then hopping away happily again as they know he is inside.
+                   The funniest recently was when a very large pheasant was in the garden doing the same and shouting at Lennie. As a result, we have added an 
+                   animation of a couple of such robins coming to harass Lennie.         
+                    <br /> 
+                    <br /> 
+                    The design and animations on this page have been created using SVG (scaleable vector graphics), html, css and javascript.
+                    The design for the robins was created by referring to images of a robin and trying to recreate a representative example and in the  animation, representative movement. 
+                    <br /> 
+                    <br /> 
+                    If you have any problems with the page and animations displaying correctly please could you let me know as usual, via the Problem Feedback button at the bottom of the home page
+                    of this website. It is much appreciated as whilst all the designs are tested using developer tools across different device settings before release, sometimes the displays
+                    will not run in the intended way and it is only by knowing about it I can try and correct it.
+                    </section>
         </div>
     );
 };
--- src/components/catlennie.js
+++ src/components/catlennie.js
@@ -5,12 +5,12 @@ export default function CatLennie() {
     return (
             <div>
                 <div>                    
-<svg className="lennieIceHouseCat" version="1.1" x="0" y="0" width="100vw" height="75vh" viewBox="-80 -90 500 560">
+<svg className="lennieIceHouseCat" version="1.1" x="0" y="0" width="100vw" height="60vh" viewBox="-80 -90 500 500">
 
            
             <g className="lennieCat" 
             xmlns="http://www.w3.org/2000/svg">  
-            </g>
+            
             <defs>
                         <linearGradient id="gradBody" x1="0%" y1="0%" x2="100%" y2="0%">
                             <stop offset="0%" style= {{ stopColor:"#5C4033", stopOpacity: 1}} />
@@ -61,16 +61,21 @@ export default function CatLennie() {
               </linearGradient>
             
             </defs>
-
- <g class="lennieLegs">
-                    <rect id="lennieLegleft" x="240" y="270" width="65" height="15" rx="5" ry="5" fill="url(#gradLegs)"/>
+          ;         <g className="heaterPad"><ellipse id="#heaterPad" cx="200" cy="290" rx="80" ry="35" fill="#A21C19" opacity="0.8"/></g>
+                    <g className="lennieLegs">
+                    <rect id="lennieLegLeft" x="240" y="270" width="65" height="15" rx="5" ry="5" fill="url(#gradLegs)"/>
                     <rect id="lennieLegRight" x="240" y="290" width="75" height="15" rx="5" ry="5" fill="url(#gradLegs)"/> 
                     </g>
                    
                     <ellipse id="catTail" cx="190" cy="299" rx="50" ry="25" fill="url(#gradBody)" stroke="#5c4033" stokeWidth="1" transform="rotate(12,187,299)"/>
                     <ellipse id="catBody" cx="200" cy="280" rx="60" ry="30" fill="url(#gradBody)" stroke="#5c4033" strokeWidth="1" />
-                   
-                    <g id="catHeadTotal">
+                    
+                    <g className="catFeeding">
+                      <rect id="catBowl" x="250" y="305" rx="30" ry="30" height="30" width="40" fill="#C2B280" / >
+                        <ellipse id="catFood" cx="270" cy="320" rx="12" ry="10" fill="#5c4033" />
+                    </g>
+                    
+                    <g className="catHeadTotal">
                     
                     <ellipse id="catHead" cx="260" cy="275" rx="25" ry="23" fill="url(#gradHead)" stroke="none" transform="rotate(0,30,0)"/>
                     
@@ -107,18 +112,98 @@ export default function CatLennie() {
                      <line id="mouthLeft" x1="263.5" y1="293" x2="269.5" y2="292" stroke="#5c4033" strokeWidth="1.5" opacity="0.7" />
                      <rect id="mouthTongue" x="260.4" y="291.5" width="9.5" height="11" rx="5" ry="5" fill="#F19E93" stroke="black" stroke-width="0.5"/>
                     </g>;
-
-                   <g>
+                    
+                    </g>
+                   <g className="iceHouse">
                     <g id="iceHousetop"><path d ="M 30 350 A 2 2 0 1 1 380 350" fill="url(#gradIceHouse)" filter="url(#blur1)" stroke="#efefe3" strokeWidth="0.2" opacity="1"></path></g>
                     
 <defs>
-                        <radialGradient id="gradIceHouse" x1="0%" y1="100%" x2="100%" y2="0%" opacity="1">
+                        <radialGradient id="gradIceHouse" x1="0%" y1="100%" x2="100%" y2="0%" opacity="1.0">
                         <stop offset="0%" style={{stopColor: "#cdced5", stopOpacity: "0"}} > </stop>
-                        <stop offset="60%" style={{stopColor: "#FFFAFA", stopOpacity: "0.7"}} > </stop>  
-                        <stop offset="80%" style={{stopColor: "white", stopOpacity: "1"}} ></stop> 
+                        <stop offset="70%" style={{stopColor: "#FFFAFA", stopOpacity: "0.7"}} > </stop>  
+                        <stop offset="95%" style={{stopColor: "white", stopOpacity: "1"}} ></stop> 
                         </radialGradient></defs></g>
-    
+                    
+                     <g className="robin1" style={{transform:"scale(0.8)"}}  filter="url(#blur8)">
+                      <defs>
+                      <filter id="blur8" x="0" y="0">
+                      <feGaussianBlur in="SourceGraphic" stdDeviation="0.5" />
+                      </filter>
+                      </defs>
+
+                      {/* <ellipse id="robin1Body" cx="0" cy="350" rx="20" ry="10" fill="#C3BDBD"" transform="translate(-200) rotate(-30)"/> */}
+                      <g id="robin1breast"><path d="M -30 270 A 1 1 10 1 0 0 260" stroke="#C3BDBD"  strokeWidth="1'5" fill= "#C3BDBD" opacity="0.8" /></g>
+                      <g id="robin1wing"><path d="M -30 270 A 0.5 0.9 40 1 1 0 260" stroke="#90816A"  strokeWidth="1'5" fill= "#90816A" opacity="0.8" /></g>
+                      <g id="robin1redbreast"><ellipse cx="-3" cy="260" rx="6" ry="10" stroke="#FF8F02"  strokeWidth="1'5" fill= "#FF8F02" opacity="0.8"/></g>
+                      <g id="robin1Tail"  transform="rotate(-40,-45, 280)"><rect x="-40" y="280" width="25" height="4" stroke="#C3BDBD"  strokeWidth="1'5" fill= "#90816A" opacity="0.8"></rect></g>
+                      <g id="robin1TailFly"  transform="rotate(55,-45, 237)"><rect x="-33" y="237" width="20" height="4" stroke="#C3BDBD"  strokeWidth="1'5" fill= "#90816A" opacity="0.8"></rect></g>
+                      <g id="robin1eye"><circle cx="-5" cy="255" r="2.0" fill="black" opacity="0.7"></circle></g>
+                      
+
+                      <g id="robin1beak"><path d="M 4 256 q 3 -2 -3 0 " fill="90816" stroke="black"  strokeWidth="2.5" opacity="0.8"  ></path></g>
+                      
+                      
+                      <g id="robin1leftLeg"><path d="M -18 280 L -18 292 Z" fill="black" stroke="black"  strokeWidth="1.7" opacity="0.8" />
+                      <g id="robin1leftFoot">
+                      <g id="robin1leftFootToe1"><path d="M -18 292 L -22 291 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1.5" opacity="0.8" /></g>
+                      <g id="robin1leftFootToe2"><path d="M -18 292 L -14 288 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      <g id="robin1leftFootToe3"><path d="M -18 292 L -14 292 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      <g id="robin1leftFootToe4"><path d="M -18 292 L -14 296 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      </g>
+                      </g>
+                      
+                      <g id="robin1rightLeg"><path d="M -12 280 L -12 292 Z" fill="black" stroke="black"  strokeWidth="1.7" opacity="0.8"/>
+                      <g id="robin1rightFoot">
+                      <g id="robin1rightFootToe1"><path d="M -12 292 L -16 291 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      <g id="robin1rightFootToe2"><path d="M -12 292 L -8 288 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      <g id="robin1rightFootToe3"><path d="M -12 292 L -8 292 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      <g id="robin1rightFootToe4"><path d="M -12 292 L -8 296 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="0.8" /></g>
+                      </g>
+                      </g>
+                      
+                      </g> 
+
+                      <g className="robin2" style={{transform:"scale(0.89)"}}  filter="url(#blur8)">
+                      <defs>
+                      <filter id="blur8" x="0" y="0">
+                      <feGaussianBlur in="SourceGraphic" stdDeviation="0.5" />
+                      </filter>
+                      </defs>
+
+                      {/* <ellipse id="robin1Body" cx="0" cy="350" rx="20" ry="10" fill="#C3BDBD"" transform="translate(-200) rotate(-30)"/> */}
+                      <g id="robin2breast"><path d="M -30 270 A 1 1 10 1 0 0 260" stroke="#C3BDBD"  strokeWidth="1'5" fill= "#C3BDBD" opacity="1.0" /></g>
+                      <g id="robin2wing"><path d="M -30 270 A 0.5 0.9 40 1 1 0 260" stroke="#90816A"  strokeWidth="1'5" fill= "#90816A" opacity="1.0" /></g>
+                      <g id="robin2redbreast"><ellipse cx="-3" cy="260" rx="6" ry="10" stroke="#FF8F02"  strokeWidth="1'5" fill= "#FF8F02" opacity="1.0"/></g>
+                      <g id="robin2Tail"  transform="rotate(-40,-45, 280)"><rect x="-40" y="280" width="25" height="4" stroke="#C3BDBD"  strokeWidth="1'5" fill= "#90816A" opacity="1.0"></rect></g>
+                      <g id="robin2TailFly"  transform="rotate(55,-45, 237)"><rect x="-33" y="237" width="20" height="4" stroke="#C3BDBD"  strokeWidth="1'5" fill= "#90816A" opacity="1.0"></rect></g>
+                      <g id="robin2eye"><circle cx="-5" cy="255" r="2.0" fill="black" opacity="0.7"></circle></g>
+                      
+
+                      <g id="robin2beak"><path d="M 4 256 q 3 -2 -3 0 " fill="90816" stroke="black"  strokeWidth="2.5" opacity="1.0"  ></path></g>
+                      
+                      
+                      <g id="robin2leftLeg"><path d="M -18 280 L -18 292 Z" fill="black" stroke="black"  strokeWidth="1.7" opacity="1.0" />
+                      <g id="robin2leftFoot">
+                      <g id="robin2leftFootToe1"><path d="M -18 292 L -22 291 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1.5" opacity="1.0" /></g>
+                      <g id="robin2leftFootToe2"><path d="M -18 292 L -14 288 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      <g id="robin2leftFootToe3"><path d="M -18 292 L -14 292 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      <g id="robin2leftFootToe4"><path d="M -18 292 L -14 296 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      </g>
+                      </g>
+                      
+                      <g id="robin2rightLeg"><path d="M -12 280 L -12 292 Z" fill="black" stroke="black"  strokeWidth="1.7" opacity="1.0"/>
+                      <g id="robin2rightFoot">
+                      <g id="robin2rightFootToe1"><path d="M -12 292 L -16 291 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      <g id="robin2rightFootToe2"><path d="M -12 292 L -8 288 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      <g id="robin2rightFootToe3"><path d="M -12 292 L -8 292 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      <g id="robin2rightFootToe4"><path d="M -12 292 L -8 296 Z" fill="#C3BDBD" stroke="black"  strokeWidth="1" opacity="1.0" /></g>
+                      </g>
+                      </g>
+                      
+                      </g> 
+
               </svg>
+
 
             </div>
             </div>

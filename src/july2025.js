2a22cd7190cd3f140364ce4241cc013e7d7f631d
Jill <125391549+JillThomas799@users.noreply.github.com>
2025-07-01 15:44:21 +0100

    Addition July blog page introduction

--- src/july2025.js
+++ src/july2025.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./july2025.css";
-// import Sunflowers2V3 from "./imagesresized/Sunflowers2V3.JPG";
-// import Sunflowers1 from "./imagesresized/Sunflowers1.jpg";
 
 export default function July2025() {
   return (
@@ -17,7 +15,7 @@ export default function July2025() {
             </Link>
           </button>
           <div id="july2025Title">July 2025</div>
-          <div className="sunflowerTiles"> </div>
+          <div className="roseTiles"> </div>
         </div>{" "}
         {/*Closing div for July2025header */}
         <div className="datenewsDivJuly2025">
@@ -25,7 +23,26 @@ export default function July2025() {
           <section className="newsHeaderJuly2025">
             Welcome to our July 2025 blog
           </section>
-          <section className="newsJuly2025"></section>
+          <section className="newsJuly2025">
+            Welcome to our July blog. Thankfully so far this year, the weather
+            has been good so let's hope that continues this month as there is
+            lots going on.
+            <br />
+            <br />
+            It is a very exciting month this month as we will be travelling down
+            to Cardiff for Harry's PhD graduation ceremony, then having friends
+            to stay and finally, we are off to the South East of England for a
+            few days including a visit to{" "}
+            <Link
+              to="https://www.glyndebourne.com/"
+              target="_blank"
+              className="linkJuly2025"
+            >
+              Glyndebourne
+            </Link>{" "}
+            in East Sussex to see Verdi's opera Falstaff, the last of his 26
+            operas, which we have never seen before.
+          </section>
         </div>
       </div>
       {/*Closing div for backgroundColorJuly2025 */}
